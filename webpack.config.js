4f5d5bf8d3edee0174eb440ade902714cdc3e0b5
Matteo Vesprini-Heidrich <mvesprinih@gmail.com>
2018-07-14 11:38:54 -0400

    initial

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "draw.js"),
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [require("@babel/plugin-proposal-object-rest-spread")]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "source-map"
+};
