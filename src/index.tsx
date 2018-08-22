bc6e38beee6e26f21bf1599f096e03043bbf502d
Matteo Vesprini-Heidrich <mvesprinih@gmail.com>
2018-08-22 19:28:56 -0400

    refactor sketch canvas component for clarity

--- src/index.tsx
+++ src/index.tsx
@@ -7,76 +7,76 @@ import sketches from "./sketches/index";
 const width = window.innerWidth * 0.95;
 const height = width * 0.625;
 
+class App extends React.Component<{}, {}> {
+  renderLinks() {
+    return sketches.map(
+      (
+        sketch: {
+          path: string;
+          linkName: string;
+          drawFunc: (width: number, height: number, svgContainer: any) => void;
+        },
+        index: number
+      ) => {
+        return (
+          <Link
+            key={index}
+            to={sketch.path}
+            className="instruction-description"
+          >
+            {sketch.linkName}
+          </Link>
+        );
+      }
+    );
+  }
+
+  renderRoutes() {
+    return sketches.map(
+      (
+        sketch: {
+          path: string;
+          drawFunc: (width: number, height: number, svgContainer: any) => void;
+        },
+        index: number
+      ) => {
+        return (
+          <Route
+            key={index}
+            path={sketch.path}
+            render={props => (
+              <SketchCanvas
+                width={width}
+                height={height}
+                drawFunc={sketch.drawFunc}
+              />
+            )}
+          />
+        );
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <nav>{this.renderLinks()}</nav>
+        </div>
+        <div>
+          <Switch>
+            {this.renderRoutes()}
+            <Redirect from="/" to="/star" />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
 render(
   <MemoryRouter>
     <App />
   </MemoryRouter>,
   document.getElementById("root")
 );
-
-function App() {
-  return (
-    <div>
-      <div>
-        <nav>
-          {sketches.map(
-            (
-              sketch: {
-                path: string;
-                linkName: string;
-                drawFunc: (
-                  width: number,
-                  height: number,
-                  svgContainer: any
-                ) => void;
-              },
-              index: number
-            ) => {
-              return (
-                <Link
-                  key={index}
-                  to={sketch.path}
-                  className="instruction-description"
-                >
-                  {sketch.linkName}
-                </Link>
-              );
-            }
-          )}
-        </nav>
-      </div>
-      <div>
-        <Switch>
-          {sketches.map(
-            (
-              sketch: {
-                path: string;
-                drawFunc: (
-                  width: number,
-                  height: number,
-                  svgContainer: any
-                ) => void;
-              },
-              index: number
-            ) => {
-              return (
-                <Route
-                  key={index}
-                  path={sketch.path}
-                  render={props => (
-                    <SketchCanvas
-                      width={width}
-                      height={height}
-                      drawFunc={sketch.drawFunc}
-                    />
-                  )}
-                />
-              );
-            }
-          )}
-          <Redirect from="/" to="/star" />
-        </Switch>
-      </div>
-    </div>
-  );
-}
