74bfc98476ecf8cb16d813e62dfc07ded1293344
Matteo Vesprini-Heidrich <mvesprinih@gmail.com>
2020-06-01 23:01:40 -0400

    emphasize glitch pattern in arpeggio sketch

--- src/sketches/arpeggio.ts
+++ src/sketches/arpeggio.ts
@@ -5,10 +5,10 @@ export default function drawArpeggio(
   height: number,
   canvasContext: CanvasRenderingContext2D
 ): void {
-  const arpeggioHeight = 100;
-  const arpeggioWidth = 100;
+  const arpeggioHeight = 40;
+  const arpeggioWidth = 40;
 
-  for (let x = 0; x < (width * height) / 3500; x++) {
+  for (let x = 0; x < (width * height) / 5000; x++) {
     const arpeggioOrigin: Point = generatePoint(width, height);
     while (
       arpeggioOrigin[0] + arpeggioWidth > 0 &&
@@ -19,12 +19,12 @@ export default function drawArpeggio(
         [arpeggioOrigin[0] + arpeggioWidth, arpeggioOrigin[1] + arpeggioHeight],
       ]);
 
-      arpeggioOrigin[0] = arpeggioOrigin[0] + arpeggioWidth / 7;
-      arpeggioOrigin[1] = arpeggioOrigin[1] - arpeggioHeight / 7;
+      arpeggioOrigin[0] = arpeggioOrigin[0] + arpeggioWidth / 5;
+      arpeggioOrigin[1] = arpeggioOrigin[1] - arpeggioHeight / 5;
     }
   }
 
-  for (let x = 0; x < (width * height) / 3500; x++) {
+  for (let x = 0; x < (width * height) / 5000; x++) {
     const arpeggioOrigin: Point = generatePoint(width, height);
     while (
       arpeggioOrigin[0] + arpeggioWidth > 0 &&
@@ -35,8 +35,8 @@ export default function drawArpeggio(
         [arpeggioOrigin[0] + arpeggioWidth, arpeggioOrigin[1] - arpeggioHeight],
       ]);
 
-      arpeggioOrigin[0] = arpeggioOrigin[0] - arpeggioWidth / 7;
-      arpeggioOrigin[1] = arpeggioOrigin[1] - arpeggioHeight / 7;
+      arpeggioOrigin[0] = arpeggioOrigin[0] - arpeggioWidth / 5;
+      arpeggioOrigin[1] = arpeggioOrigin[1] - arpeggioHeight / 5;
     }
   }
 }
