5d70daa388a2200539fb3c961890f53f3e8eca6c
Matteo Vesprini-Heidrich <mvesprinih@gmail.com>
2022-01-03 17:51:54 -0500

    bunch of yarn updates (from a yarn install) and other changes i'm not sure about

--- src/sketches/swirlViz.ts
+++ src/sketches/swirlViz.ts
@@ -13,14 +13,28 @@ import mp3 from "../music/this_is_how_i_wear_my_underwear.mp3";
 let lastExecuteTime: number;
 let startTime: number;
 
-let points: PolarPoint[] = [];
+const ops: Op[] = underwear.ops;
+const opVoiceSet = new Set();
+ops.forEach((op) => {
+  opVoiceSet.add(op.voice);
+});
+const VOICE_COUNT = opVoiceSet.size;
+
+let lines: PolarPoint[][] = [];
+// initialize lines to have an empty array for each voice
+for (let i = 0; i < VOICE_COUNT; i++) {
+  lines.push([]);
+}
 
 const TIME_DILATION = 0.5;
 
-const SPREAD_FACTOR = 10;
+const SPREAD_FACTOR = 50;
 
 // controls how violent abberrations appear
 const ABERRATION_FACTOR = 8;
+
+// higher numbers create higher aberrations due to amplitude changes
+const MAGNITUDE_ABERRATION_FACTOR = 10;
 
 // how quickly sounds fall off, closer to 1 sustains visualization affection longer
 const ABERRATION_DAMPING_FACTOR = 0.97;
@@ -35,11 +49,9 @@ interface Op {
   l: number;
 }
 
-const ops: Op[] = underwear.ops;
-
 let audio: HTMLAudioElement;
 
-let lastPos: PolarPoint = { magnitude: 0, rotation: 0 };
+let lastPositions: PolarPoint[] = [];
 
 export default function drawSwirlViz(
   width: number,
@@ -50,7 +62,6 @@ export default function drawSwirlViz(
 ): void {
   if (audio === undefined) {
     audio = new Audio(mp3);
-    console.log(audio);
     audio.play();
   }
 
@@ -68,59 +79,103 @@ export default function drawSwirlViz(
 
   const center: Point = [width / 2, height / 2];
 
-  const opsToConsider = [];
+  const opsToConsider = new Map<number, Op[]>();
   if (ops.length > 0 && ops[0] !== undefined) {
     const newOps = ops;
     while (ops[0].t < t) {
-      opsToConsider.push(ops.shift());
+      const op = ops.shift();
+      if (opsToConsider.has(op.voice)) {
+        const newOpArray = [...opsToConsider.get(op.voice), op];
+        opsToConsider.set(op.voice, newOpArray);
+      } else {
+        opsToConsider.set(op.voice, [op]);
+      }
     }
   }
 
-  const maxAmplitude = opsToConsider.reduce((prevMax, op) => {
-    return Math.max(prevMax, op.z);
-  }, 0);
+  for (let i = 0; i < VOICE_COUNT; i++) {
+    if (!opsToConsider.has(i) && lines[i].length === 0) {
+      continue;
+    }
 
-  const maxPan = opsToConsider.reduce((prevMax, op) => {
-    return Math.abs(op.x) > Math.abs(prevMax) ? op.x : prevMax;
-  }, 0);
+    const opsInVoice = opsToConsider.has(i) ? opsToConsider.get(i) : [];
 
-  const newMaxAmp = Math.max(maxAmplitude, lastPos.magnitude);
-  const newMaxRotation =
-    Math.abs(maxPan) > Math.abs(lastPos.rotation) ? maxPan : lastPos.rotation;
-  const p: PolarPoint = {
-    magnitude:
-      generateNumberInRange(0.9 * newMaxAmp, newMaxAmp) * ABERRATION_FACTOR,
-    rotation:
-      generateNumberInRange(0.9 * newMaxRotation, newMaxRotation) *
-      ABERRATION_FACTOR,
-  };
+    const maxAmplitude = opsInVoice.reduce((prevMax, op) => {
+      return Math.max(prevMax, op.z);
+    }, 0);
 
-  const periodAdjustedAberration = 1 - (1 - ABERRATION_DAMPING_FACTOR) * delta;
-  lastPos = {
-    magnitude: newMaxAmp * periodAdjustedAberration,
-    rotation: newMaxRotation * periodAdjustedAberration,
-  };
+    const maxPan = opsInVoice.reduce((prevMax, op) => {
+      return Math.abs(op.x) > Math.abs(prevMax) ? op.x : prevMax;
+    }, 0);
 
-  points = points.map((p, i) => {
-    return {
-      magnitude: p.magnitude + delta * TIME_DILATION * SPREAD_FACTOR,
-      rotation: p.rotation + delta * TIME_DILATION * Math.PI,
+    const lastPosAmp = lastPositions[i] ? lastPositions[i].magnitude : 0;
+    const lastPosRot = lastPositions[i] ? lastPositions[i].rotation : 0;
+    const newMaxAmp = Math.max(maxAmplitude, lastPosAmp);
+    const newMaxRotation =
+      Math.abs(maxPan) > Math.abs(lastPosRot) ? maxPan : lastPosRot;
+
+    const VOICE_COUNT_NORMALIZED_SPREAD = 0.5 * i + 1;
+    const p: PolarPoint = {
+      magnitude:
+        generateNumberInRange(0.9 * newMaxAmp, newMaxAmp) *
+        ABERRATION_FACTOR *
+        MAGNITUDE_ABERRATION_FACTOR *
+        VOICE_COUNT_NORMALIZED_SPREAD,
+      rotation:
+        generateNumberInRange(0.9 * newMaxRotation, newMaxRotation) *
+        ABERRATION_FACTOR,
     };
-  });
-  points = [p, ...points];
 
-  const cartesianPoints = points.map((p) => {
-    const cartesianPoint = polarToCartesian(p, center);
-    const driftLimit = (0.5 * t) / 1000;
-    return generateNearbyPoint(width, height, cartesianPoint, {
-      posY: driftLimit,
-      negY: driftLimit,
-      posX: driftLimit,
-      negX: driftLimit,
+    const periodAdjustedAberration =
+      1 - (1 - ABERRATION_DAMPING_FACTOR) * delta;
+    lastPositions[i] = {
+      magnitude: newMaxAmp * periodAdjustedAberration,
+      rotation: newMaxRotation * periodAdjustedAberration,
+    };
+
+    lines[i] = lines[i].map((p) => {
+      const magnitude = p.magnitude + delta * TIME_DILATION * SPREAD_FACTOR;
+      const rotation = p.rotation + delta * TIME_DILATION * Math.PI;
+
+      if (magnitude > width / 2 && magnitude > height / 2) {
+        return undefined;
+      }
+
+      return {
+        magnitude,
+        rotation,
+      };
     });
-  });
 
-  appendLine(canvasContext, cartesianPoints);
+    const firstUndefined = lines[i].indexOf(undefined);
+    if (firstUndefined >= 0) {
+      lines[i].splice(firstUndefined, lines[i].length - firstUndefined);
+    }
+
+    // trim points that are rendered offscreen anyway
+    for (let x = lines[i].length - 1; x >= 0; x--) {
+      if (lines[i][x] === undefined) {
+        lines[i].pop();
+      } else {
+        break;
+      }
+    }
+
+    lines[i] = [p, ...lines[i]];
+
+    const cartesianPoints = lines[i].map((p) => {
+      const cartesianPoint = polarToCartesian(p, center);
+      const driftLimit = (0.5 * t) / 1000;
+      return generateNearbyPoint(width, height, cartesianPoint, {
+        posY: driftLimit,
+        negY: driftLimit,
+        posX: driftLimit,
+        negX: driftLimit,
+      });
+    });
+
+    appendLine(canvasContext, cartesianPoints);
+  }
 
   requestAnimationFrame((newTime) => {
     delta = newTime - lastExecuteTime;
